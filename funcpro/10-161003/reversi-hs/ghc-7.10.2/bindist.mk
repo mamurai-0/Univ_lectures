a3f2f58ab050cf5951a5e6ed55461a34dd30fcc3
mamurai-0 <gohome.x105.gn@gmail.com>
2017-02-22 12:23:12 +0900

    I erased huge files which I accidentally added

--- funcpro/10-161003/reversi-hs/ghc-7.10.2/bindist.mk
+++
@@ -1,7 +0,0 @@
-HADDOCK_DOCS       = YES
-BUILD_DOCBOOK_HTML = YES
-BUILD_DOCBOOK_PS   = NO
-BUILD_DOCBOOK_PDF  = NO
-BUILD_MAN          = YES
-override ghc-cabal_INPLACE = utils/ghc-cabal/dist-install/build/tmp/ghc-cabal-bindist
-UseSystemLibFFI    = NO
--- funcpro/10-161003/reversi-hs/ghc-7.10.2/utils/ghc-pkg/dist-install/build/Version.hs
+++
@@ -1,5 +0,0 @@
-module Version where
-version, targetOS, targetARCH :: String
-version    = "7.10.2"
-targetOS   = "darwin"
-targetARCH = "x86_64"
